8ddacf325dfb66ad1a00b6c28ad3f400b06108b2
Roie Engelstein <roiee@campus.technion.ac.il>
2025-12-16 00:54:38 +0200

    Add equasion operators. remove copying capability

--- Pair.h
+++ Pair.h
@@ -4,4 +4,9 @@ struct Pair {
     T2 second;
 
     Pair(const T1& first, const T2& second) : first(first), second(second) {}
+    Pair(Const Pair& otehr) = delete;
+    Pair& operator=(const pair& other) = delete;
+    operator<(const Pair& other){return this->first < other.first;}
+    operator>(const Pair& other){return this->first > other.first;}
+    operator==(const Pair& other){return this->first == other.first;}
 };
