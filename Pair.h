5ebffb61b17a90ec3e295ab466701a219c33eefd
Roie Engelstein <roiee@campus.technion.ac.il>
2025-12-16 10:51:50 +0200

    mend methods

--- Pair.h
+++ Pair.h
@@ -6,13 +6,13 @@ struct Pair {
     T2 second;
 
     Pair(const T1& first, const T2& second) : first(first), second(second) {}
-    Pair(Const Pair& otehr) = default;
+    Pair(const Pair& otehr) = default;
     Pair& operator=(const Pair& other) = delete;
     bool operator<(const Pair& other) const {return this->first < other.first;}
     bool operator>(const Pair& other) const {return this->first > other.first;}
     bool operator==(const Pair& other) const {return this->first == other.first;}
-    friend std::ostream& operator<<(std::ostream& out, const Pair&) const{
-        out << "(" << first << "," << second << ")";
+    friend std::ostream& operator<<(std::ostream& out, const Pair& pair) const{
+        out << "(" << pair.first << "," << pair.second << ")";
         return out; 
     }
 };
