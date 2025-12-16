003d120bb4b4da389b67fcf16deed6b6d138cf9a
Roie Engelstein <roiee@campus.technion.ac.il>
2025-12-16 11:02:38 +0200

    mend syntax errors

--- Dict.h
+++ Dict.h
@@ -5,33 +5,33 @@
 
 template <typename T>
 class Dict{
-  AvlTree<Pair<const int, shared_ptr<T>> tree;
+  AvlTree<Pair<const int, std::shared_ptr<T>> tree;
 
   public:
   Dict() = default;
   ~Dict() = default;
   Dict(Dict& other) = delete;
-  operator=() = delete;
+  Dict& operator=(const Dict& other) = delete;
 
   void Add(const int id, T& data);
   T& Get(const int id);
   void Remove(const int id);
-  const shared_ptr<T>& GetShared(const int id) const;
+  const std::shared_ptr<T>& GetShared(const int id) const;
 
   private:
-  Pair MakeDummy(const int id) const;
+  Pair<const int, std::shared_ptr<T>> MakeDummy(const int id) const;
 
   //for debug only:
 
   public:
   void PrintInOrder();
-}
+};
 
 //puclic funcs:
 
 template<typename T>
 void Dict<T>::Add(const int id, T& data){
-  Pair<const int, shared_ptr<T>> temp = Pair(id, std::make_shared<T>(new T(data)));
+  Pair<const int, std::shared_ptr<T>> temp = Pair<const int, sts::shared_ptr<T>>(id, std::make_shared<T>(new T(data)));
   try{
     tree.Add(temp);
   }
@@ -43,7 +43,7 @@ void Dict<T>::Add(const int id, T& data){
 
 template<typename T>
 T& Dict<T>::Get(const int id){
-  Pair<const int, shared_ptr<T>> temp = MakeDummy(id);
+  Pair<const int, std::shared_ptr<T>> temp = MakeDummy(id);
   try{
     return tree.Find(temp).second;
   }
@@ -54,7 +54,7 @@ T& Dict<T>::Get(const int id){
 
 template<typename T>
 void Dict<T>::Remove(const int id){
-  Pair<const int, shared_ptr<T>> temp = MakeDummy(id);
+  Pair<const int, std::shared_ptr<T>> temp = MakeDummy(id);
   try{
     tree.Remove(temp);
   }
@@ -65,7 +65,7 @@ void Dict<T>::Remove(const int id){
 
 template<typename T>
 const shared_ptr<T>& Dict::GetShared(const int id) const{
-  Pair<const int, shared_ptr<T>> temp = MakeDummy(id);
+  Pair<const int, std::shared_ptr<T>> temp = MakeDummy(id);
   try{
     return tree.Find(temp).second;
   }
